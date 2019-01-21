2d8cbce51d70f857a284d710d480b717003ed007
wqlab0000 <w.q.lab00@gmail.com>
2019-01-21 02:34:22 +0000

    Update Test.java

--- srcbdd/Test.java
+++ srcbdd/Test.java
@@ -6,60 +6,22 @@ import java.util.InputMismatchException;
 
 public class Test{
     
-    //public static boolean mg = false;
-    //Patient patient = new Patient();
-    
     public static void main(String[] args) {
         
-//        Patient patient1 = new Patient();
-//        patient1.stepPatient(300);
-//        patient1.show();
+        Patient patient1 = new Patient();
+        InsulinPump insulinPump1 = new InsulinPump();
         
-//        InsulinPump insulinPump = new InsulinPump();
-//        InsulinPump1.stepInsulinPump(3.2);
-//
-        
-//        Timer timer = new Timer();
-//        timer.schedule(new show(), 300 * 1000);
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Glucose level：");
         int glucose = sc.nextInt();
         System.out.println("Insulin dose：");
         float dose = sc.nextFloat();
-//        System.out.println("请输入你的工资：");
-//        float salary = sc.nextFloat();
-        System.out.println("你的信息如下：");
+       
+        System.out.println("Information is shown below：");
         System.out.println("Glucose level is："+glucose+"\n"+"Insulin dose："+dose);
-        
-         System.out.print("Enter a Char:");
-        
-        
-        System.out.println("your char is :");
-
-
-
-        System.out.println("Glucose level should be over 144");
+      System.out.println("Glucose level should be over 70");
         
         
-        //Patient glucose level
-        
-        //        Patient patient1 = new Patient();
-        //        Patient patient2 = new Patient();
-        //
-        //
-        //       InsulinPump insulinPump1 = new InsulinPump();
-        //       InsulinPump insulinPump2 = new InsulinPump();
-        //
-        //
-        //        InsulinPump.isOn();
-        //        patient1.insulin(0.32f,1.0f);
-        //        patient2.insulin(0.02f,3.0f);
-        //        patient2.foodIntake(0.02f,3.0f);
-        //
-        //        patient1.show();
-        //        patient2.show();
-        //
-        
+       
     }
 }
