32efb019885b768419ddd117726f0dc5d11de421
wqlab0000 <w.q.lab00@gmail.com>
2019-01-21 02:44:28 +0000

    Update Clock.java

--- srcbdd/Clock.java
+++ srcbdd/Clock.java
@@ -2,6 +2,12 @@ import java.util.*;
 import java.math.*;
 import static java.lang.System.out;
 import java.util.InputMismatchException;
+import org.junit.runner.RunWith;
+
+RunWith(Clock.class)
+Cucumber.Options(
+format = {"pretty", "json:target/"},
+features = {"src/insulinPump/"})
 
 
 
