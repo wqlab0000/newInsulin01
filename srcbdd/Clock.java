46f028cdbca764a191264adae16c08cb0a5b9157
wqlab0000 <w.q.lab00@gmail.com>
2019-01-21 02:38:06 +0000

    Update Clock.java

--- srcbdd/Clock.java
+++ srcbdd/Clock.java
@@ -5,33 +5,31 @@ import java.util.InputMismatchException;
 
 
 
-//Given("^insulin pump is turned on$")
+@Given("^insulin pump is turned on$")
 
 public class Clock extends Thread{
     InsulinPump insulinPump1 = new InsulinPump();
     Patient patient1 = new Patient();
 
-
-    private int second; //@ in _time;
-    //@ private invariant 0 <= read;
-    private int read; //@ in _time;
-    private /*@ spec_public @*/ double insulinDose;
-    private /*@ spec_public @*/ double basal_rate=0.5;
-    private static double startTime = System.currentTimeMillis();
+    private int second; 
+  
+    private double insulinDose;
+    private double basal_rate=0.5;
+    private static double startTime = System.currentTimeMillis();// System current time.
 
 
-//When("^insulin pump glucose reading level time is setted$")
+@When("^insulin pump glucose reading level time is setted$")
    
     
 
-    public /*@ pure @*/ int getSecond(){
+    public int getSecond(){
         return second=0;
         
     }
 
 
 
-//Then("^insulin pump reads glucse every n (\\d+) seconds$")
+@Then("^insulin pump reads glucse every n (\\d+) seconds$")
 
     public void stepClock() {
         startTime = System.currentTimeMillis();
