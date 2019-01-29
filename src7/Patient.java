7c344142cdabc56b784e3f4064dd6093df7195ec
wqlab0000 <w.q.lab00@gmail.com>
2019-01-29 02:30:44 +0000

    Add files via upload

---
+++ src7/Patient.java
@@ -0,0 +1,58 @@
+public class Patient {
+    private double glucoseLevel=140;
+    
+    private double insulinDose=0.5;
+    
+
+    
+    public double readGlucoseLevel() {
+        return glucoseLevel;
+    }
+    
+    public double readPatientInsulin() {
+        return insulinDose;
+    }
+    
+    public static boolean foodIntake()
+    {
+        return true;
+    }
+
+    public static boolean exercise()
+    {
+        return true;
+    }
+   
+    public void setFoodIntake(double glucoseLevel)
+    {
+        glucoseLevel=250;
+        glucoseLevel++;
+    }
+    
+    public void setExercise(double glucoseLevel)
+    {
+        glucoseLevel--;
+    }
+   
+    
+    public void setInsulinDose(double insulinDose) {
+        insulinDose ++;
+    }
+    
+    
+    public void stepPatient(){
+        readPatientInsulin();
+        readGlucoseLevel();
+    }
+    
+    public double setGlucoseLevel(double glucoseLevel){
+        this.glucoseLevel=glucoseLevel;
+        readGlucoseLevel();
+        return glucoseLevel;
+    }
+    
+    
+    
+    
+   
+}
