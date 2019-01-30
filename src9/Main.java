da60d369e61e669e7415e03c83aa3a51caf8ff07
wqlab0000 <w.q.lab00@gmail.com>
2019-01-30 02:42:21 +0000

    Add files via upload

---
+++ src9/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        InsulinPump insulinPump = new InsulinPump();
+
+        System.out.println("------------------------------------");
+        
+       
+        System.out.println("Insulin Pump ON");
+        
+        System.out.println("check glucose level? Input y for yes");
+        Scanner s=new Scanner(System.in);
+        String checking=s.nextLine();
+        if(checking.equals("y"))
+        {
+        insulinPump.stepInsulinPump();
+        
+        insulinPump.getInsulinPumpStatus();
+        }
+      
+        
+        
+        System.out.println("Insulin Pump OFF");
+
+        System.out.println("------------------------------------");
+                
+                
+        
+    }
+}
