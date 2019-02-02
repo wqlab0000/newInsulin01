2d5d4bc58639e6b6309329898ce881cb1cbbcbc1
wqlab0000 <w.q.lab00@gmail.com>
2019-02-02 02:45:23 +0000

    Update Stepdefs.java

--- src/hellocucumber/src/test/java/hellocucumber/Stepdefs.java
+++ src/hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -5,15 +5,6 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import static org.junit.Assert.*;
 
-// class InsulinPump {
-//     static String insulinPump(String status) {
-// 	if (status.equals("OFF")) {
-// 	    return "basalDose";
-// 	}
-//     return "noDose";
-//     }
-// }
-
 public class Stepdefs {
     private String status;
     private String actualDose;
