179505db1e046d840c750b4d75c5cd44f4d5d872
wqlab0000 <w.q.lab00@gmail.com>
2019-02-03 19:49:34 +0000

    Update and rename Test.java to InsulinPump.java

---
+++ srckey/InsulinPump.java
@@ -0,0 +1,21 @@
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.When;
+//import cucumber.api.java.en.Then;
+//import static org.junit.Assert.*;
+
+
+class InsulinPump {
+/*@  requires status == "OFF";
+  @  ensures \result == "basalDose";
+  @  also
+  @  requires status != "OFF";
+  @  ensures \result == "noDose";
+  @*/
+
+    static /*@pure@*/ String insulinPump(String status) {
+	if (status.equals("OFF")) {
+	    return "basalDose";
+	}
+    return "noDose";
+    }
+}
