7431ea4fbd8febccbbacbb31a31c68d0ab40d3f1
wqlab0000 <w.q.lab00@gmail.com>
2019-02-24 12:45:20 +0000

    Update InsulinPump.java

--- src/hellocucumber/src/test/java/hellocucumber/InsulinPump.java
+++ src/hellocucumber/src/test/java/hellocucumber/InsulinPump.java
@@ -1,22 +1,12 @@
 package hellocucumber;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
-import cucumber.api.java.en.Then;
-import static org.junit.Assert.*;
-
-class InsulinPump {
-/*@  requires status == "OFF";
+public class InsulinPump {
+/*@  public normal_behavior
   @  ensures \result == "basalDose";
-  @  also
-  @  requires status != "OFF";
-  @  ensures \result == "noDose";
+  @
   @*/
 
-    static String insulinPump(String status) {
-	if (status.equals("OFF")) {
-	    return "basalDose";
-	}
-    return "noDose";
-    }
+public static String insulinPump(String OFF) {
+      return "basalDose";
+  }
 }
