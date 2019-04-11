8911660283747f21c7e55c1b84dc7c0f07079766
wqlab0000 <w.q.lab00@gmail.com>
2019-04-11 21:23:22 +0900

    Update InsulinPump.java

--- src/hellocucumber/src/test/java/hellocucumber/InsulinPump.java
+++ src/hellocucumber/src/test/java/hellocucumber/InsulinPump.java
@@ -1,12 +1,24 @@
 package hellocucumber;
+class InsulinPump {
+  /*@  public normal_behavior
+    @ requires glucose >= 250;
+    @ ensures \result == "2";
+    @ also
+    @ requires glucose < 250 && glucose > 120 ;
+    @ ensures \result == "0.5";
+    @ also
+    @ requires glucose <= 120 ;
+    @ ensures \result == "0";
+    @*/
 
-public class InsulinPump {
-/*@  public normal_behavior
-  @  ensures \result == "basalDose";
-  @
-  @*/
-
-public static String insulinPump(String OFF) {
-      return "basalDose";
+    static String insulinPump(String glucose) {
+	if (glucose >=250) {
+	    return "2";
+	}
+	if (glucose < 250 && glucose > 120){
+      return "0.5";
   }
+      return "0";
 }
+}
+
